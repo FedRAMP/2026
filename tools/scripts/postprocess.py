05422bb5b069ac427c1d7c49a66c7933c482803c
pete-gov <pete.waterman@gsa.gov>
2026-06-19 18:32:32 -0400

    or this

--- tools/scripts/postprocess.py
+++ tools/scripts/postprocess.py
@@ -2,8 +2,8 @@ from plugin import postprocess_site
 
 if __name__ == "__main__":
     postprocess_site(
-        site_dir="html",  # Your build output directory
-        docs_dir="src",  # Your source docs directory
+        site_dir="../../html",  # Your build output directory
+        docs_dir="../../src",  # Your source docs directory
         site_url="https://fedramp.gov/2026",
         default_image="https://www.fedramp.gov/feature-background.jpg",
         default_author="pete@fedramp.gov",
