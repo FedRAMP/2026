6840be40661103d6defb975788184b1e8db6b19d
pete-gov <pete.waterman@gsa.gov>
2026-06-19 18:31:01 -0400

    does this build previews properly?

--- tools/scripts/postprocess.py
+++ tools/scripts/postprocess.py
@@ -3,9 +3,9 @@ from plugin import postprocess_site
 if __name__ == "__main__":
     postprocess_site(
         site_dir="html",  # Your build output directory
-        docs_dir="markdown",  # Your source docs directory
-        site_url="https://fedramp.gov/join",
-        default_image="https://www.fedramp.gov/join/assets/frcs.jpg",
+        docs_dir="src",  # Your source docs directory
+        site_url="https://fedramp.gov/2026",
+        default_image="https://www.fedramp.gov/feature-background.jpg",
         default_author="pete@fedramp.gov",
         add_desc=True,
         add_image=True,
