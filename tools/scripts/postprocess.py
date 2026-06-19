1e66b18283f2b21ba129ddcbd9f7aaf93efff894
pete-gov <pete.waterman@gsa.gov>
2026-06-19 18:47:11 -0400

    updated preview image!

--- tools/scripts/postprocess.py
+++ tools/scripts/postprocess.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
         site_dir="../html",  # Your build output directory
         docs_dir="../src",  # Your source docs directory
         site_url="https://fedramp.gov/2026",
-        default_image="https://www.fedramp.gov/feature-background.jpg",
+        default_image="https://preview.fedramp.gov/2026/assets/preview.png",
         default_author="pete@fedramp.gov",
         add_desc=True,
         add_image=True,
